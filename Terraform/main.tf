2846bfec4c1d0d2135294dca8c558558c2924f61
ShebinPaul <48907562+ShebinPaul@users.noreply.github.com>
2022-08-02 17:42:27 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,21 +2,18 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      Version = "~>3.27"
+      version = "4.22.0"
     }
   }
-  required_version = ">=0.14.9"
-  backend "s3" {
+}
+  
+backend "s3" {
        bucket = "ivnc-test-access"
        #key    = "[Remote_State_S3_Bucket_Key]"
        region = "east-us-1"
    }
-}
 
-provider "aws" {
-  version = "~>3.0"
-  region  = "east-us-1"
-}
+
 
 resource "aws_s3_bucket" "s3Bucket" {
      bucket = "githubtest12345"
