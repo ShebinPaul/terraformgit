7a244575c8df2f65d1981b26ffe0612103d2e9fc
ShebinPaul <48907562+ShebinPaul@users.noreply.github.com>
2022-08-03 19:38:54 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -14,22 +14,6 @@ provider "aws" {
 
 resource "aws_s3_bucket" "s3Bucket" {
      bucket = "githubtest12345"
-     acl       = "public-read"
-
-     policy  = <<EOF
-  {
-	"id": "MakePublic",
-	"version": "2012-10-17",
-	"statement": [{
-		"action": [
-			"s3:GetObject"
-		],
-		"effect": "Allow",
-		"resource": "arn:aws:s3:::githubtest12345/*",
-		"principal": "*"
-	}]
-}
-EOF
 website {
        index_document = "index.html"
    }
