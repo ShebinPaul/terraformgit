5cc7a841b460d2f44d325fedd4bcc759698edea7
ShebinPaul <48907562+ShebinPaul@users.noreply.github.com>
2022-08-03 19:30:27 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,11 +1,16 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.22.0"
-    }
-  }
+provider "aws" {
+  version = "~>3.0"
+  region  = "us-east-1"
 }
+
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "4.22.0"
+#     }
+#   }
+# }
 
 resource "aws_s3_bucket" "s3Bucket" {
      bucket = "githubtest12345"
